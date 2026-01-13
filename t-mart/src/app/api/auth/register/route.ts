e006c3ca54fae368a24787e7fedd60655cdd303a
Tejas mehar <tejas mehar7@gmail.com>
2026-01-13 10:04:00 +0530

    Created Register api

---
+++ t-mart/src/app/api/auth/register/route.ts
@@ -0,0 +1,44 @@
+import connectDB from "@/config/db";
+import { NextRequest, NextResponse } from "next/server";
+import User from "../../../../models/user.model";
+import bcrypt from "bcryptjs";
+
+export async function POST(req: NextRequest) {
+  try {
+    await connectDB();
+    const { name, email, password } = await req.json();
+    const existUser = await User.findOne({ email });
+    if (existUser) {
+      return NextResponse.json({
+        message: "Email Already Exist",
+        status: 400,
+      });
+    }
+
+    if (password.length < 6) {
+      return NextResponse.json({
+        message: "Password Must be 6 Character",
+        status: 400,
+      });
+    }
+
+    const hashpassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashpassword,
+    });
+
+    return NextResponse.json({
+      user,
+      status: 200,
+    });
+
+  } catch (err) {
+            return NextResponse.json({
+            message:`register err ${err} `,
+            status:400
+        })
+  }
+}
